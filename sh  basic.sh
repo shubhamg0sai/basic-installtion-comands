53d052cda89fbe1fcbd0cc89b31c1bfdea7fc519
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-09-29 08:49:02 +0530

    Create sh  basic.sh

---
+++ sh  basic.sh
@@ -0,0 +1,63 @@
+echo ""
+echo ""
+echo ""
+echo "The script is  made by ShuBhamg0sain"
+echo ""
+echo "Facebook  : https://m.facebook.com/shubham.gosain.980"
+echo "Telegram : https://t.me/ShuBham_g0sain"
+echo "follow me on Instagram https://www.instagram.com/shubham_g0sain/?hl=en"
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+apt-get update -y                    
+apt install python -y           
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+apt install screenfetch -y
+apt install cowsay -y
+apt install toilet -y
+apt install figlet -y
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+pkg install openssh -y
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install fish -y
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+apt install tor -y
+apt install w3m -y
+apt install cmatrix -y
+apt install wcalc -y 
+apt install openssl -y
+apt install bmon -y
+apt-get upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "Be a Hacker...."
+echo ""
+echo ""
+echo "Facebook  : https://m.facebook.com/shubham.gosain.980"
+echo "Telegram : https://t.me/ShuBham_g0sain"
+echo "follow me on Instagram https://www.instagram.com/shubham_g0sain/?hl=en"
+exit
