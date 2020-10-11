b351c66ba9e08076753448c1179a406d22941527
sHuBhAm_G0sAiN <63947251+ShuBhamg0sain@users.noreply.github.com>
2020-10-11 19:36:23 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -13,6 +13,305 @@ echo "Installtion process is started"
 echo ""
 echo ""
 echo ""
+𝑻𝒆𝒓𝒎𝒖𝒙 𝑩𝒂𝒔𝒊𝒄 𝑪𝒐𝒎𝒎𝒂𝒏𝒅𝒔 
+
+
+pkg install libgmp-dev -y 
+
+pkg insall libpcap-dev -y 
+
+pkg install postgresql-dev -y 
+
+pkg install readline-dev -y  
+
+pkg install libsqlite-dev -y 
+
+pkg install openssl-dev -y
+
+pkg install libxml2-dev -y 
+
+pkg install libxslt-dev -y 
+
+pkg install ncurses-dev -y 
+
+pkg install ruby-dev -y 
+
+pkg install libgrpc-dev -y
+
+pkg update -y 
+
+pkg upgrade -y 
+
+Pkg install git -y  
+
+pkg install python2 -y 
+
+pkg install php 
+
+pkg install openssh 
+
+pkg install curl 
+
+pkg install python 
+
+pkg install nano 
+
+pkg install toilet 
+
+pkg install perl 
+
+pkg install nmap 
+
+pkg install clang 
+
+pkg install bash 
+
+pkg install w3m  
+
+pkg install hydra 
+
+pkg install figlet  
+
+pkg install tar 
+
+pkg install unzip 
+
+pkg install tor 
+
+pkg install wget 
+
+pkg install wcalc 
+
+pkg install bmon 
+
+pkg install proot 
+
+pkg install toilet 
+
+pkg install net-tools &&
+
+pkg install golang termux-chroot
+
+pkg install cmatrix 
+
+pkg install macchanger 
+
+pkg install ruby 
+
+pkg install fish 
+
+pkg install help
+
+pip2 install --upgrade pip
+
+pip2 install requests 
+
+pip2 install Mechanize 
+
+pip2 install termcolor 
+
+gem install lolcat 
+
+pkg install libffi-dev -y 
+
+pkg install libgmp-dev -y 
+
+pkg insall libpcap-dev -y 
+
+pkg install postgresql-dev -y 
+
+pkg install readline-dev -y 
+
+pkg install libsqlite-dev -y 
+
+pkg install openssl-dev -y 
+
+pkg install libxml2-dev -y 
+
+pkg install libxslt-dev -y 
+
+pkg install ncurses-dev -y 
+
+pkg install ruby-dev -y 
+
+pkg install libgrpc-dev -y 
+
+pkg update -y
+
+pkg upgrade -y 
+
+pkg install git -y 
+
+pkg install python2 -y 
+
+pkg install php 
+
+pkg install openssh 
+
+pkg install curl 
+
+pkg install python 
+
+pkg install nano 
+
+pkg install toilet 
+
+pkg install perl 
+
+pkg install nmap 
+
+pkg install clang 
+
+pkg install bash 
+
+pkg install w3m  
+
+pkg install hydra 
+
+pkg install figlet  
+
+pkg install tar 
+
+pkg install unzip 
+
+pkg install tor 
+
+pkg install wget 
+
+pkg install wcalc 
+
+pkg install bmon 
+
+pkg install proot 
+
+pkg install toilet 
+
+pkg install net-tools 
+
+pkg install golang termux-chroot 
+
+pkg install cmatrix 
+
+pkg install macchanger 
+
+pkg install ruby
+ 
+pkg install fish 
+
+pkg install help
+
+pip2 install --upgrade pip 
+
+pip2 install requests 
+
+pip2 install Mechanize 
+
+pip2 install termcolor
+
+gem install lolcat
+ 
+apt install bash
+
+pkg update -y
+
+pkg upgrade -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python2-dev -y
+
+pkg install python3 -y
+
+pkg install java -y
+
+pkg install fish -y
+
+pkg install ruby -y
+
+pkg install help -y
+
+pkg install git -y
+
+pkg install host -y
+
+pkg install php -y
+
+pkg install perl -y
+
+pkg install nmap -y
+
+pkg install bash -y
+
+pkg install clang -y
+
+pkg install nano -y
+
+pkg install w3m -y
+
+pkg install havij -y
+
+pkg install hydra -y
+
+pkg install figlet -y
+
+pkg install cowsay -y
+
+pkg install curl -y
+
+pkg install tar -y
+
+pkg install zip -y
+
+pkg install unzip -y
+
+pkg install tor -y
+
+pkg install google -y
+
+pkg install sudo -y
+
+pkg install wget -y
+
+pkg install wireshark -y
+
+pkg install wgetrc -y 
+
+pkg install wcalc -y
+
+pkg install bmon -y
+
+pkg install vpn -y
+
+pkg install unrar -y
+
+pkg install toilet -y
+
+pkg install proot -y
+
+pkg install net-tools -y
+
+pkg install golang -y
+
+pkg install chroot -y
+
+pkg install macchanger -y
+
+pkg install openssl -y
+
+pkg install cmatrix -y
+
+pkg install openssh -y
+
+pkg install wireshark -y
+
+termux-setup-storage -y
+
+pkg install macchanger -y
+
+apt update && apt upgrade -y
+
 
 apt-get update -y                    
 apt install python -y           
