ac23aead06f81acb595129466a2acd00dfcc2e35
BLACK-KILLER <63947251+ShuBhamg0sain@users.noreply.github.com>
2021-05-23 17:20:21 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -7,41 +7,41 @@ echo ""
 echo -e "\e[1;32m Installtion process is started" 
 echo ""
 echo -e "\e[1;32m 𝑻𝒆𝒓𝒎𝒖𝒙 𝑩𝒂𝒔𝒊𝒄 𝑪𝒐𝒎𝒎𝒂𝒏𝒅𝒔"
-pkg update -y 
-pkg upgrade -y 
-Pkg install git -y  
-pkg install python2 -y 
-pkg install php 
-pkg install openssh 
-pkg install curl 
-pkg install python 
-pkg install nano 
-pkg install toilet 
-pkg install perl 
-pkg install nmap 
-pkg install clang 
-pkg install bash 
-pkg install w3m  
-pkg install hydra 
-pkg install figlet  
-pkg install tar 
-pkg install root-repo
-pkg install unstable-repo
-pkg install x11-repo
-pkg install unzip 
-pkg install tor 
-pkg install wget 
-pkg install wcalc 
-pkg install bmon 
-pkg install proot 
-pkg install toilet 
-pkg install net-tools &&
-pkg install golang termux-chroot
-pkg install cmatrix 
-pkg install macchanger 
-pkg install ruby 
-pkg install fish 
-pkg install help
+apt update -y 
+apt upgrade -y 
+apt install git -y  
+apt install python2 -y 
+apt install php 
+apt install openssh 
+apt install curl 
+apt install python 
+apt install nano 
+apt install toilet 
+apt install perl 
+apt install nmap 
+apt install clang 
+apt install bash 
+apt install w3m  
+apt install hydra 
+apt install figlet  
+apt install tar 
+apt install root-repo
+apt install unstable-repo
+apt install x11-repo
+apt install unzip 
+apt install tor 
+apt install wget 
+apt install wcalc 
+apt install bmon 
+apt install proot 
+apt install toilet 
+apt install net-tools &&
+apt install golang termux-chroot
+apt install cmatrix 
+apt install macchanger 
+apt install ruby 
+apt install fish 
+apt install help
 pip2 install --upgrade pip
 pip2 install requests 
 pip2 install Mechanize 
@@ -50,93 +50,93 @@ gem install lolcat
 pip install -r requirements.txt
 pip2 install requirements.txt
 pip2 install mechnaize
-pkg update -y
-pkg upgrade -y 
-pkg install git -y 
-pkg install python2 -y 
-pkg install php 
-pkg install openssh 
-pkg install curl 
-pkg install python 
-pkg install nano 
-pkg install toilet 
-pkg install perl 
-pkg install nmap 
-pkg install clang 
-pkg install bash 
-pkg install w3m  
-pkg install hydra 
-pkg install figlet  
-pkg install tar 
-pkg install unzip 
-pkg install tor 
-pkg install wget 
-pkg install wcalc 
-pkg install bmon 
-pkg install proot 
-pkg install toilet 
-pkg install net-tools 
-pkg install golang termux-chroot 
-pkg install cmatrix 
-pkg install macchanger 
-pkg install ruby
-pkg install fish 
-pkg install help
+apt update -y
+apt upgrade -y 
+apt install git -y 
+apt install python2 -y 
+apt install php 
+apt install openssh 
+apt install curl 
+apt install python 
+apt install nano 
+apt install toilet 
+apt install perl 
+apt install nmap 
+apt install clang 
+apt install bash 
+apt install w3m  
+apt install hydra 
+apt install figlet  
+apt install tar 
+apt install unzip 
+apt install tor 
+apt install wget 
+apt install wcalc 
+apt install bmon 
+apt install proot 
+apt install toilet 
+apt install net-tools 
+apt install golang termux-chroot 
+apt install cmatrix 
+apt install macchanger 
+apt install ruby
+apt install fish 
+apt install help
 pip2 install --upgrade pip 
 pip2 install requests 
 pip2 install Mechanize 
 pip2 install termcolor
 gem install lolcat
 apt install bash
-pkg update -y
-pkg upgrade -y
-pkg install python -y
-pkg install python2 -y
-pkg install python2-dev -y
-pkg install python3 -y
-pkg install java -y
-pkg install fish -y
-pkg install ruby -y
-pkg install help -y
-pkg install git -y
-pkg install host -y
-pkg install php -y
-pkg install perl -y
-pkg install nmap -y
-pkg install bash -y
-pkg install clang -y
-pkg install nano -y
-pkg install w3m -y
-pkg install havij -y
-pkg install hydra -y
-pkg install figlet -y
-pkg install cowsay -y
-pkg install curl -y
-pkg install tar -y
-pkg install zip -y
-pkg install unzip -y
-pkg install tor -y
-pkg install google -y
-pkg install sudo -y
-pkg install wget -y
-pkg install wireshark -y
-pkg install wgetrc -y 
-pkg install wcalc -y
-pkg install bmon -y
-pkg install vpn -y
-pkg install unrar -y
-pkg install toilet -y
-pkg install proot -y
-pkg install net-tools -y
-pkg install golang -y
-pkg install chroot -y
-pkg install macchanger -y
-pkg install openssl -y
-pkg install cmatrix -y
-pkg install openssh -y
-pkg install wireshark -y
+apt update -y
+apt upgrade -y
+apt install python -y
+apt install python2 -y
+apt install python2-dev -y
+apt install python3 -y
+apt install java -y
+apt install fish -y
+apt install ruby -y
+apt install help -y
+apt install git -y
+apt install host -y
+apt install php -y
+apt install perl -y
+apt install nmap -y
+apt install bash -y
+apt install clang -y
+apt install nano -y
+apt install w3m -y
+apt install havij -y
+apt install hydra -y
+apt install figlet -y
+apt install cowsay -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+apt install unzip -y
+apt install tor -y
+apt install google -y
+apt install sudo -y
+apt install wget -y
+apt install wireshark -y
+apt install wgetrc -y 
+apt install wcalc -y
+apt install bmon -y
+apt install vpn -y
+apt install unrar -y
+apt install toilet -y
+apt install proot -y
+apt install net-tools -y
+apt install golang -y
+apt install chroot -y
+apt install macchanger -y
+apt install openssl -y
+apt install cmatrix -y
+apt install openssh -y
+apt install wireshark -y
 termux-setup-storage -y
-pkg install macchanger -y
+apt install macchanger -y
 git clone https://github.com/ShuBhamg0sain/FIND-ME
 cd FIND-ME
 bash Find.sh
